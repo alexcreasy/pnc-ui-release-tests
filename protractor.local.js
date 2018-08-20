be69e74e17ecfb5953b443a2f788905ab907fb25
Alex Creasy <acreasy@redhat.com>
2018-08-20 12:44:51 +0100

    WAYPOINT - cleanup reports

--- protractor.local.js
+++ protractor.local.js
@@ -1,31 +1,9 @@
 const JasmineReporters = require('jasmine-reporters');
-const HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
 const testSuites = require('./suites.json');
 const testParams = require('./params');
 
-const reporter = new HtmlScreenshotReporter({
-    dest: 'reports',
-    filename: 'testrun-report.html',
-    showSummary: true,
-    showQuickLinks: true,
-    reportOnlyFailedSpecs: false,
-    captureOnlyFailedSpecs: false
-});
-
 
 exports.config = {
-    plugins: [
-        {
-            package: 'protractor-screenshoter-plugin',
-            screenshotPath: './REPORTS/e2e',
-            screenshotOnExpect: 'failure+success',
-            screenshotOnSpec: 'none',
-            withLogs: true,
-            writeReportFreq: 'asap',
-            imageToAscii: 'none',
-            clearFoldersBeforeTest: true
-        }
-    ],
     allScriptsTimeout: 10 * 60 * 1000,
     capabilities: {
         directConnect: true,
@@ -35,37 +13,31 @@ exports.config = {
         }
     },
     directConnect: true,
-    framework: 'jasmine',
+    framework: 'jasmine2',
     getPageTimeout: 20 * 1000,
     jasmineNodeOpts: {
         showColors: true,
         defaultTimeoutInterval: 10 * 60 * 1000
     },
-    beforeLaunch: () => {
-        process.on('uncaughtException', () => {
-            reporter.jasmineDone();
-            reporter.afterLaunch();
-        });
-
-        return new Promise(resolve => {
-            reporter.beforeLaunch(resolve);
-        });
-    },
-    onPrepare: () => {
-//        jasmine.getEnv().afterAll(done => setTimeout(done, 3000));
-        
+    onPrepare: () => {        
         jasmine.getEnv().addReporter(new JasmineReporters.JUnitXmlReporter({
             savePath: 'reports',
             consolidateAll: false
         }));
-
-        jasmine.getEnv().addReporter(reporter);
-    },
-    
-    afterLaunch: exitCode => {
-        return new Promise(resolve => reporter.afterLaunch(resolve.bind(this, exitCode)));
     },
     params: testParams,
+    plugins: [
+        {
+            package: 'protractor-screenshoter-plugin',
+            screenshotPath: './reports/e2e',
+            screenshotOnExpect: 'failure+success',
+            screenshotOnSpec: 'failure+success',
+            withLogs: true,
+            writeReportFreq: 'asap',
+            imageToAscii: 'none',
+            clearFoldersBeforeTest: true
+        }
+    ],
     SELENIUM_PROMISE_MANAGER: false,
     suites: testSuites
 };
